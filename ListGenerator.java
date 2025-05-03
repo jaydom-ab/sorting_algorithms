46c06b2b42f1f30926644eb26ee7f783511f1fbd
jaydom-ab <juliandominic.abonal@gmail.com>
2025-05-03 01:26:37 +0000

    Added List Generator and Insertion Sort

---
+++ ListGenerator.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ListGenerator{
+    public ArrayList<Integer> generate(int i){
+        ArrayList<Integer> list = new ArrayList<>();
+        Random r = new Random();
+        for (int x = 0; x < 15; x++){
+            list.add(r.nextInt(101));
+        }
+        return list;
+    }
+}
+
+
